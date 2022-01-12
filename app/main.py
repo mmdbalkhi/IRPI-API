c30c50e033e46b68dc52896ae3ec34494554909a
mmdbalkhi <ahmdparsh129@gmail.com>
2022-01-13 00:01:26 +0330

    add find_img_date func
    this func can find and return json file datas

--- app/main.py
+++ app/main.py
@@ -10,59 +10,68 @@
     
     * The Images have copyright license *
 """
-import random
 import os
+import random
+from json import load
+from typing import Dict
+from uuid import uuid4
 
 from flask import Config, Flask, Markup, abort, jsonify, request
+
 app = Flask(__name__, static_folder='img')
 
 app.secret_key = "dev"
 
 image_path = os.getcwd()+"/img" if "app" in os.getcwd() else os.getcwd()+"/app/img/"
 
-def place(): #TODO: work it
-    return None
+def find_img_date(img_name:str) -> Dict:
+    
+    try:
+        with open(f"{image_path}{img_name}/info.json") as file:
+            return load(file)
+    except:
+        return
+
 
 @app.route("/v1/random", methods=["GET"])
 def v1_random():
+
     img_name = random.choice(os.listdir(image_path))
-    img_place = place()
-    img_path = f"{request.host_url}/img/{img_name}"
+    img_data = find_img_date(img_name)
+    img_path = f"{request.host_url}img/{img_name}/image.jpg" # FIX: HARD CODE! 
 
     return jsonify(
         {
+            "id":uuid4(),
             "status": "ok",
             "image_name" : img_name,
-            "place" : img_place,
+            "data" : img_data,
             "image_path" : img_path
         }
     ), 200
     
 @app.route("/v1/find/<img_name>", methods=["GET"])
 def v1_by_name(img_name):
-    
-    if ".jpg" not in img_name:
-        img_name += ".jpg"
 
-    if os.path.exists(f"{image_path}{img_name}"):
-        img_place = place()
-        img_path = f"{request.host_url}img/{img_name}"
+
+    if os.path.exists(f"{image_path}{img_name}/image.jpg"):
+        img_data = find_img_date(img_name)
+        img_path = f"{request.host_url}img/{img_name}/image.jpg"
 
         return jsonify(
             {
+                "id":uuid4(),
                 "status": "ok",
                 "img name" : img_name,
-                "place" : img_place,
+                "data" : img_data,
                 "img url" : img_path
             }
         ), 200
     return jsonify(
             {
+                "id":uuid4(),
                 "status" : "failed",
                 "img name" : img_name,
                 "msg" : "img not exists"
             }
         ), 404
-
-
-app.run(debug=True)
