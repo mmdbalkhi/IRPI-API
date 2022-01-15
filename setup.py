67b9c99675e509efead626c8d6d25123a15b7353
mmdbalkhi <ahmdparsh129@gmail.com>
2022-01-15 10:33:48 +0330

    Lint the codes

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+import sys
+
 from setuptools import setup
+
+if sys.version_info < (3, 6, 0):
+    sys.exit(
+        "Python 3.6 or later is required. "
+        "See https://github.com/pypa/pipx "
+        "for installation instructions."
+    )
 
 if __name__ == "__main__":
     setup()
