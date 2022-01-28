f47d54da858ed5773b63de0ec28de3d6b404aee3
mmdbalkhi <ahmdparsh129@gmail.com>
2022-01-28 13:36:19 +0330

    edit search_with_regex func

--- IRIP/utils.py
+++ IRIP/utils.py
@@ -15,7 +15,7 @@ def find_img_date(img_name: str) -> Dict:
     Returns:
         Dict: the json data from the file
     """
-    path = f"{image_path}{img_name}"
+    path = f"{image_path}{img_name}"  # TODO: img_name = path in img directory
     try:
         with open(f"{path}/info.json", encoding="utf-8") as file:
             return json_load(file)
@@ -23,5 +23,5 @@ def find_img_date(img_name: str) -> Dict:
         return
 
 
-def search_with_regex():
-    pass
+def search_with_regex(name: str) -> None:
+    return
