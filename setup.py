e2d163fa5cd3d18f3ed309b254013d3a61387c1c
komeil Parseh <ahmdparsh129@gmail.com>
2022-06-03 14:56:30 +0430

    python36 => python 37

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-
 import sys
 
 from setuptools import setup  # type: ignore
 
-if sys.version_info < (3, 6, 0):
-    sys.exit("Python 3.6 or later is required. ")
+if sys.version_info < (3, 7, 0):
+    sys.exit("Python 3.7 or later is required. ")
 
 setup(install_requires=["flask"])
